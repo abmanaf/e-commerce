7058cc0407f24c65335ed114f19c9b88c3b5b724
Alibaba <abdulmanafaliu414@gmail.com>
2023-12-24 10:47:44 +0000

    i removed some line of code in the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js or vite.config.ts
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-})
+};
