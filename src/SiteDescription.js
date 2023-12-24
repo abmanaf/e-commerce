e92dba9046afc7b717ec0284d92d03874d720063
Alibaba <abdulmanafaliu414@gmail.com>
2023-12-24 09:14:39 +0000

    changes made

---
+++ src/SiteDescription.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SiteDescription = () => {
+  return (
+    <div>
+      <p style={{ textAlign: "center" }}>SiteDescription Page Comming soon</p>
+    </div>
+  );
+};
